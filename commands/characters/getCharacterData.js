b62471ed99cebaf3cd86c25a1cb07a0afc363336
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-31 11:11:26 -0300

    refactor: changed the way that agent abilities are displayed

--- commands/characters/getCharacterData.js
+++ commands/characters/getCharacterData.js
@@ -5,6 +5,8 @@ const {
   StringSelectMenuBuilder,
   ActionRowBuilder,
   EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
 } = require("discord.js");
 
 async function getValorantCharacters() {
@@ -65,20 +67,68 @@ module.exports = {
         return { name: ability.displayName, value: ability.description };
       });
 
+      const agentAbilitiesDisplay = new ButtonBuilder()
+        .setCustomId("abilities")
+        .setLabel(`Habilidades do(a) agente ${agent.displayName}`)
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji("🛠️");
+
+      const rows = new ActionRowBuilder().addComponents(agentAbilitiesDisplay);
+
       const embed = () =>
         new EmbedBuilder()
-          .setTitle(`Confira dados do(a) agente ${agent.displayName}`)
+          .setTitle(agent.displayName)
           .setDescription(agent.description)
-          .addFields(...abilities)
+
           .setImage(agent.fullPortrait)
           .setAuthor({
             name: interaction.user.username,
             iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png`,
           })
+          .setURL(
+            `https://playvalorant.com/pt-br/agents/${String(
+              agent.displayName
+            ).toLowerCase()}/`
+          )
+          .setThumbnail(agent.displayIcon)
           .setColor("Green")
           .setTimestamp(new Date().getTime());
 
-      return await option.reply({ embeds: [embed()] });
+      const agentResponse = await option.reply({
+        embeds: [embed()],
+        components: [rows],
+      });
+      const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+      try {
+        const sent = await response.awaitMessageComponent({
+          filter: collectorFilter,
+        });
+
+        return await sent.update({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle(agent.displayName)
+              .setColor("Green")
+              .setDescription(
+                `${agent.displayName} é um (uma) ${agent.role.displayName}. ${agent.role.description}`
+              )
+              .setThumbnail(agent.displayIcon)
+              .addFields({ name: "Função", value: agent.role.displayName })
+              .addFields(...abilities)
+              .setFooter({
+                text: agent.role.displayName,
+                iconURL: agent.role.displayIcon,
+              })
+              .setURL(
+                `https://playvalorant.com/pt-br/agents/${String(
+                  agent.displayName
+                ).toLowerCase()}/`
+              ),
+          ],
+          components: [],
+        });
+      } catch (err) {}
     } catch (err) {
       console.log(err);
     }
